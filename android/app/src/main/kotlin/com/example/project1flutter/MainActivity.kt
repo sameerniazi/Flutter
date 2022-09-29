f5e25793a75ebb6f801dd7194ebc193163bb3d60
Sameer <sameerniazi8266@gmail.com>
2022-09-29 14:28:35 +0500

    Initial commit
    Signed-off-by: Sameer <sameerniazi8266@gmail.com>

--- android/app/src/main/kotlin/com/example/project1flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/project1flutter/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.project1flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home:Text("Hi From Sameer")
-  ));
+      home: Scaffold(
+    appBar: AppBar(
+      title: Text('Title Of An App'),
+    ),
+    body: Center(
+      child: Text('Body text of the app'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text("Click"),
+    ),
+  )));
 }
-
-
