8e86cd0d5b0939969cdaf1d0fcf25b70903ca72a
Sameer <sameerniazi8266@gmail.com>
2022-09-29 14:28:35 +0500

    Initial commit
    Signed-off-by: Sameer <sameerniazi8266@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home:Text("Hi From Sameer")
-  ));
+      home: Scaffold(
+    appBar: AppBar(
+      title: Text('Title Of An App'),
+    ),
+    body: Center(
+      child: Text(
+        'Body text of the app',
+        style: TextStyle(
+            color: Colors.green,
+            fontSize: 30.0,
+            fontFamily: 'Passion',
+            letterSpacing: 2.0,
+            fontWeight: FontWeight.bold),
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text("Click"),
+    ),
+  )));
 }
-
-
